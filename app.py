9152e2e77befd996dd4f66a34903880a91e82ce6
pmemmons <pmemmons@iu.edu>
2024-03-23 00:03:38 -0400

    done!

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/greet')
 def greet():
-    return '<h1>Hello, my bug Carmela! I love you so much! 💖</h1>'
+    return 'This is a special message for Carmela!'
 
 if __name__ == '__main__':
     app.run(debug=True)
